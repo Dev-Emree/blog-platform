c873206914ab807cc9f929a76ed028d20f5392a4
Emre Ekiz <dev.emree+github@gmail.com>
2024-03-16 00:56:23 +0300

    Unnecessary codes removed

--- index.js
+++ index.js
@@ -16,25 +16,6 @@ app.use(logger('dev'));
 
 app.set('view engine', 'ejs');
 
-const isProduction = process.env.NODE_ENV.toLowerCase() === 'production';
-
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error('Veritabanı bağlantı hatası:', err);
-        if (isProduction) {
-            process.exit(1);
-        }
-    } else {
-        console.log('Veritabanı bağlantısı başarılı');
-    }
-});
 
 app.use('/', mainRouter);
 
